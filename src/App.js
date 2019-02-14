506b2a035f6b40e284c57f6d770c97a5296f5c65
bizhe <bijeorxan@gmail.com>
2019-02-14 17:02:05 +0400

    types and reducers

--- src/App.js
+++ src/App.js
@@ -4,37 +4,16 @@ import "./App.css";
 
 import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
-import { createStore, applyMiddleware } from "redux";
-
-const store = createStore(() => {}, {}, applyMiddleware());
+import store from "./Store";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
-  componentWillMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(data => this.setState({ posts: data }));
-  }
-
   render() {
-    const postItems = this.state.posts.map(post => (
-      <div key={post.id}>
-        <h3>{post.title}</h3>
-        <p>{post.body}</p>
-      </div>
-    ));
     return (
       <Provider store={store}>
         <div>
           <PostForm />
           <hr />
           <Posts />
-          {postItems}
         </div>
       </Provider>
     );
